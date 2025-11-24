fa57a0edd7f6def987a4a3bb4826966cfd49cf14
Baojun-Che <1833437064@qq.com>
2025-11-24 14:52:38 +0800

    fix sbatch file for Darcy problem

--- Derivative-Free-Variational-Inference/GMBBVI_Darcy.jl
+++ Derivative-Free-Variational-Inference/GMBBVI_Darcy.jl
@@ -1,9 +1,15 @@
-using  MATLAB, MAT, Random, LinearAlgebra
+using MATLAB, MAT, Random, LinearAlgebra, Distributed
 include("../Inversion/GMBBVI.jl")
+include("../Inversion/AnnealingInitialize.jl")
 include("../Benchmark-Darcy/mean_ref.jl")
 
-mat"addpath('./Benchmark-Darcy')"
-mat"load('precomputations.mat')"
+
+script_dir = pwd()
+mat"cd($script_dir)"
+mat"addpath('../Benchmark-Darcy')"
+mat"load('../Benchmark-Darcy/precomputations.mat')"
+
+@info "MATLAB files loaded successfully"
 
 function func_Phi(x)
     theta = exp.(x)
@@ -29,11 +35,12 @@ end
 
 Random.seed!(123);
 
-N_modes = 20 # number of modes in Gaussian mixture
-N_x = 64
-N_ens = 4*N_x
+N_x = 64 # dimension of Darcy problem (fixed)
+N_modes = 1  # number of modes in Gaussian mixture
+N_ens = 8*N_x 
+N_iter = 1000  
+dt = 0.5
 
-fig, ax = PyPlot.subplots(nrows=4, ncols=6, sharex=false, sharey=false, figsize=(30,18))
 
 σ_0 = 2.0
 x0_w  = ones(N_modes)/N_modes
@@ -44,23 +51,34 @@ for im = 1:N_modes
     xx0_cov[im, :, :] .= Σ0
 end
 
-
-N_iter = 40
-dt = 0.5
+@info "Running: Darcy problem"
+@info "N_modes = ", N_modes, "N_iter = ", N_iter, "N_ens = ", N_ens
 
 # annealing process
-# x0_w_anneal, x0_mean_anneal, xx0_cov_anneal = initialize_with_annealing(func_Phi, x0_w, x0_mean, xx0_cov, N_iter = 0)
-
+# x0_w_anneal, x0_mean_anneal, xx0_cov_anneal = initialize_with_annealing(func_Phi, x0_w, x0_mean, xx0_cov, N_iter = 1)
 x0_w_anneal, x0_mean_anneal, xx0_cov_anneal = x0_w, x0_mean, xx0_cov
 
 # run GMBBVI
 @time  obj = Gaussian_mixture_GMBBVI(func_Phi, x0_w_anneal, x0_mean_anneal, xx0_cov_anneal; N_iter = N_iter, dt = dt, N_ens = N_ens)
 
-for iter = 0:N_iter
-    x_w  = exp.(obj.logx_w[iter+1])
-    xx_cov  = obj.xx_cov[iter+1]
-    x_mean  = obj.x_mean[iter+1]
-    error = norm( mean_ref - compute_exp_expectation(x_w, x_mean, xx_cov) )
-    rel_error = error / norm(mean_ref)
-    println(iter," ", error," ", rel_error)
+open("data.txt", "w") do io
+    for iter = 0:N_iter
+        x_w  = exp.(obj.logx_w[iter+1])
+        xx_cov  = obj.xx_cov[iter+1]
+        x_mean  = obj.x_mean[iter+1]
+        error_vec = mean_ref - compute_exp_expectation(x_w, x_mean, xx_cov)
+        rel_error = norm(error_vec) / norm(mean_ref)
+        pre_error = norm(error_vec./mean_ref)
+        line = string(iter, " ", rel_error, " ", pre_error)
+        println(line)      # print to terminal
+        println(io, line)  # print to data.txt
+        
+        if iter == N_iter
+            println("error vector:")
+            println(error_vec)
+        end
+    end
 end
+
+mat"quit"
+exit(0)
